88ca366d222fdf65891bea1186a8aa2807f824b5
Cstew12 <92701896+Cstew12@users.noreply.github.com>
2022-01-15 22:44:59 -0800

    added submit button

--- fitfinder/src/App.js
+++ fitfinder/src/App.js
@@ -28,8 +28,8 @@ function App() {
                     
                 </FormGroup>
                 <FormGroup>
-                    <Button class= "btn btn-outline-primary p-1">Male</Button>
-                    <Button class= "btn btn-outline-primary p-1">Female</Button>
+                    <Button class= "btn btn-outline-primary m-1 p-1">Male</Button>
+                    <Button class= "btn btn-outline-primary m-1 p-1">Female</Button>
                 </FormGroup>
                 <FormGroup>
                     <Form.Label>Age</Form.Label>
@@ -46,10 +46,10 @@ function App() {
 
                 </FormGroup>
                 <FormGroup>
-                    <Button class= "btn btn-outline-secondary p-1">Less than once a month</Button>
-                    <Button class= "btn btn-outline-secondary p-1">1-3 times a month</Button>
-                    <Button class= "btn btn-outline-secondary p-1">1-3 times a week</Button>
-                    <Button class= "btn btn-outline-secondary p-1">4+ times a week</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">Less than once a month</Button>
+                    <Button class= "btn btn-outline-secondary m-1p-1">1-3 times a month</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">1-3 times a week</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">4+ times a week</Button>
                 </FormGroup>
                 <FormGroup>
                     <Form.Label>
@@ -58,9 +58,13 @@ function App() {
 
                 </FormGroup>
                 <FormGroup>
-                    <Button class= "btn btn-outline-secondary p-1">Beginner</Button>
-                    <Button class= "btn btn-outline-secondary p-1">intermediate</Button>
-                    <Button class= "btn btn-outline-secondary p-1">advanced</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">Beginner</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">intermediate</Button>
+                    <Button class= "btn btn-outline-secondary m-1 p-1">advanced</Button>
+                    
+                </FormGroup>
+                <FormGroup>
+                    <Button class= "btn btn-outline-secondary m-4 p-1">Submit</Button>
                     
                 </FormGroup>
 
